4a96daff1f3b8cdea91000a93e98a04bd9f67ff2
Rushi <rushikeshdantakalecoding@gmail.com>
2024-03-27 00:58:07 +0530

    commit1

--- DB/conn.js
+++ DB/conn.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-// Connection string from environment variable
-const DB = "mongodb+srv://Rushi9080:Rushi9080@cluster0.dnrwsii.mongodb.net/reactQuiz?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.set("strictQuery", false)
 
-mongoose.connect(DB)
-.then(() => {
-    console.log('Connection to MongoDB successful');
-})
-.catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
-});
+
+const DB =  process.env.DB;
+
+
+mongoose.connect(DB).then(()=>{
+        console.log('connection successful')
+    }).catch((err)=>{
+        console.log(err)
+    });
--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const dotenv = require('dotenv')
 const PORT = process.env.PORT || 5000 
 
 app.use(cors())
 app.use(express.json())
+dotenv.config({path:'./.env'});
+
 
 app.use(require("./Router/auth"))
 require("./DB/conn")
