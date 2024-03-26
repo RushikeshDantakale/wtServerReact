e98905fc1809e5e2e0afda4d734b72b164ab40f1
Rushi <rushikeshdantakalecoding@gmail.com>
2024-03-27 00:41:07 +0530

    minor change

--- DB/conn.js
+++ DB/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.set("strictQuery", false)
 
 
-const DB = "mongodb://localhost:27017/reactQuiz" ||  process.env.DB ;
+const DB = process.env.DB ;
 
 
 mongoose.connect(DB).then(()=>{
