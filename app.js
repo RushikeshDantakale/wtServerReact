7951f0b661e6eefc79c7dadd9d28a9cf3f682a04
Rushi <rushikeshdantakalecoding@gmail.com>
2024-03-28 22:56:34 +0530

    added body-parser in the server

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
 const PORT = process.env.PORT || 5000 
+const bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json())
 dotenv.config({path:'./.env'});
 
 
